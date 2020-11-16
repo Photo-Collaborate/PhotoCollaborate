1a8a0829d73db29060284a6fae0e8758e2c3f5ba
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-11-15 22:50:50 -0500

    Connected to parse server

--- Photo_Collaborate/Photo_Collaborate/AppDelegate.swift
+++ Photo_Collaborate/Photo_Collaborate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "UH2I21kekAYEhIaOW0qEVu2cKkTbTzBlG3i9tqfD"
+            $0.clientKey = "AajJJiFr2GsV2KtO0fkkKjhEMM0I7L6hm8Yx6gVx"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
